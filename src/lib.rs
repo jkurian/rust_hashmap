22c04918bdbcf6464ba1adf2c2d7a23bd2da2822
Jerry Kurian <jkurian770@gmail.com>
2019-01-08 09:20:11 -0500

    Basic entry API

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,54 @@ pub struct HashMap<K, V> {
     items: usize,
 }
 
+pub struct OccupiedEntry<'a, K, V> {
+    element: &'a mut (K, V),
+}
+
+pub struct VacantEntry<'a, K, V> {
+    key: K,
+    bucket: &'a mut Vec<(K, V)>,
+}
+
+impl<'a, K, V> VacantEntry<'a, K, V> {
+    pub fn insert(self, value: V) -> &'a mut V {
+        self.bucket.push((self.key, value));
+        &mut self.bucket.last_mut().unwrap().1
+    }
+}
+
+pub enum Entry<'a, K, V> {
+    Occupied(OccupiedEntry<'a, K, V>),
+    Vacant(VacantEntry<'a, K, V>),
+}
+
+impl<'a, K, V> Entry<'a, K, V> {
+    pub fn or_insert(self, value: V) -> &'a mut V {
+        match self {
+            Entry::Occupied(e) => &mut e.element.1,
+            Entry::Vacant(e) => e.insert(value),
+        }
+    }
+    pub fn or_insert_with<F>(self, maker: F) -> &'a mut V
+    where
+        F: FnOnce() -> V,
+    {
+        match self {
+            Entry::Occupied(e) => &mut e.element.1,
+            Entry::Vacant(e) => e.insert(maker()),
+        }
+    }
+    pub fn or_default(self) -> &'a mut V
+    where
+        V: Default,
+    {
+        match self {
+            Entry::Occupied(e) => &mut e.element.1,
+            Entry::Vacant(e) => e.insert(V::default()),
+        }
+    }
+}
+
 impl<K, V> HashMap<K, V>
 where
     K: Hash + Eq,
@@ -108,6 +156,8 @@ where
         self.items -= 1;
         Some(bucket.swap_remove(i).1)
     }
+
+    pub fn entry(&mut self, key: K) -> Entry<K, V> {}
 }
 
 pub struct Iter<'a, K: 'a, V: 'a> {
