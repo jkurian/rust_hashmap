883766d4d9d285590acea7c66c2a8e3a57f32bdb
Jerry Kurian <jkurian770@gmail.com>
2019-01-26 10:19:10 -0500

    fmt

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+pub mod hashmap;
+pub mod hashset;
+
+pub fn main() {
+    let mut map = hashmap::HashMap::new();
+    map.insert("key", "value");
+
+    let mut set = hashset::HashSet::new();
+    set.insert(52);
+    // set.is_empty();
+    // set.get(52);
+    // set.len();
+}
